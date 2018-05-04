36908933791c6cc4e1977ee8fbb8a96b2cd28279
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-04 08:34:42 +0200

    Added "proposition" theorem environment

--- general/lua/languageSwitch.lua
+++ general/lua/languageSwitch.lua
@@ -1,14 +1,15 @@
-function multiLanguage(chapter, index, theorem, corollary, lemma, definition)
+function multiLanguage(chapter, index, theorem, corollary, lemma, definition, proposition)
   tex.print("\\renewcommand{\\chaptername}{"..chapter.."}")
   tex.print("\\renewcommand{\\contentsname}{"..index.."}")
   tex.print("\\newtheorem{theorem}{"..theorem.."}[section]")
   tex.print("\\newtheorem{corollary}{"..corollary.."}[theorem]")
   tex.print("\\newtheorem{lemma}[theorem]{"..lemma.."}")
+  tex.print("\\newtheorem{proposition}[theorem]{"..proposition.."}")
   tex.print("\\newtheorem{definition}[theorem]{"..definition.."}")
 end
 
 if metadata["language"] == "it" then
-  multiLanguage("Capitolo", "Indice", "Teorema", "Corollario", "Lemma", "Definizione")
+  multiLanguage("Capitolo", "Indice", "Teorema", "Corollario", "Lemma", "Definizione", "Proposizione")
 else
-  multiLanguage("Chapter", "Index", "Theorem", "Corollary", "Lemma", "Definition")
+  multiLanguage("Chapter", "Index", "Theorem", "Corollary", "Lemma", "Definition", "Proposition")
 end
