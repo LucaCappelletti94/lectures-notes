261a24068e04f82d8d69edf40096062d65b0fef2
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-07-19 14:01:21 +0200

    [GENERAL] Added observation env and e vector alias

--- general/lua/languageSwitch.lua
+++ general/lua/languageSwitch.lua
@@ -1,15 +1,16 @@
-function multiLanguage(chapter, index, theorem, corollary, lemma, definition, proposition)
+function multiLanguage(chapter, index, theorem, corollary, lemma, definition, proposition, observation)
   tex.print("\\renewcommand{\\chaptername}{"..chapter.."}")
   tex.print("\\renewcommand{\\contentsname}{"..index.."}")
   tex.print("\\newtheorem{theorem}{"..theorem.."}[section]")
   tex.print("\\newtheorem{corollary}{"..corollary.."}[theorem]")
   tex.print("\\newtheorem{lemma}[theorem]{"..lemma.."}")
   tex.print("\\newtheorem{proposition}[theorem]{"..proposition.."}")
+  tex.print("\\newtheorem{observation}[theorem]{"..observation.."}")
   tex.print("\\newtheorem{definition}[theorem]{"..definition.."}")
 end
 
 if metadata["language"] == "it" then
-  multiLanguage("Capitolo", "Indice", "Teorema", "Corollario", "Lemma", "Definizione", "Proposizione")
+  multiLanguage("Capitolo", "Indice", "Teorema", "Corollario", "Lemma", "Definizione", "Proposizione", "Osservazione")
 else
-  multiLanguage("Chapter", "Index", "Theorem", "Corollary", "Lemma", "Definition", "Proposition")
+  multiLanguage("Chapter", "Index", "Theorem", "Corollary", "Lemma", "Definition", "Proposition", "Observation")
 end
